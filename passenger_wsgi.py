d86af630d88623da1fd9c0a3e890315f1e2f88f5
AkashdeepArjun <wadhwaarjun007@gmail.com>
2026-07-28 11:50:14 +0530

    test

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,11 +2,10 @@ import os
 import sys
 
 # Tell Passenger explicitly which Python virtualenv to use
-INTERP = os.path.expanduser('~/virtualenv/flask_app/3.11/bin/python')
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
+# INTERP = os.path.expanduser('~/virtualenv/flask_app/3.11/bin/python')
+# if sys.executable != INTERP:
+#     os.execl(INTERP, INTERP, *sys.argv)
 
 sys.path.insert(0, os.path.dirname(__file__))
 
 from test import app as application
-#lol
