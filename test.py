9a6875de635edb3988cd9fb706038af0e6fd04f1
AkashdeepArjun <wadhwaarjun007@gmail.com>
2026-07-29 19:30:48 +0530

    fix rate limt trial

--- test.py
+++ test.py
@@ -71,7 +71,8 @@ try:
             get_remote_address,
             app=app,
             default_limits=["200 per day","50 per hour"],
-            storage_uri="memory://"
+            storage_uri="memory://",
+            enabled=True
     
         )
     #see if  works
