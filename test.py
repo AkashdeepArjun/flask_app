de8b17d06873fd9f3cba19c523aa32696b7be25a
AkashdeepArjun <wadhwaarjun007@gmail.com>
2026-08-04 20:55:31 +0530

    headers modified

--- test.py
+++ test.py
@@ -480,6 +480,16 @@ def load_current_user():
         flask.g.user = User.query.filter_by(username=name).first()
         # flask.g.user.rol
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5173'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    return response
+
+
+
 
 
 def admin_required(f):
