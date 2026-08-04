0a5476a3373a81f56338b77598ce24a302e62585
AkashdeepArjun <wadhwaarjun007@gmail.com>
2026-08-04 17:06:01 +0530

    tee

--- test.py
+++ test.py
@@ -952,7 +952,7 @@ def login_user():
             username=form.usermail.data
             userpassword = form.userpassword.data
 
-            app.logger.log(f"request from user {username} recieved")
+            app.logger.info(f"request from user {username} recieved")
                 
             user =User.query.filter_by(email=username).first()
             if user:
